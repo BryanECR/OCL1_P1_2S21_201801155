a09e0ff6285822286469a8f496a0fe65e1ae2968
Bryan <70345203+BryanECR@users.noreply.github.com>
2021-08-19 12:53:43 -0500

    Create proyect

---
+++ OLC1_P1/src/olc1_p1/OLC1_P1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1_p1;
+
+/**
+ *
+ * @author Userlen
+ */
+public class OLC1_P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
